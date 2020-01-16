f89f2232e532a7b85437149e011d41a4f59836e7
Ethan Dalool <git@voussoir.net>
2020-01-15 21:34:01 -0800

    Add cmd_autoexec.bat.

---
+++ cmd_autoexec.bat
@@ -0,0 +1,11 @@
+@echo off
+REM 1. Go to or create "HKCU\Software\Microsoft\Command Processor"
+REM 2. Create string value "Autorun"
+REM 3. Set its value to "call <path_to_this_file>"
+REM And now all of these commands will be invisibly executed in every new cmd window.
+REM I use it to set doskeys which are as close as Windows gets to unix aliases.
+
+doskey .=cd.
+doskey ..=cd..
+doskey ...=start.
+doskey \=cd\
