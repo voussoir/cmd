5806930198cb163054e0693c6f4493486653f658
Ethan Dalool <git@voussoir.net>
2020-01-19 10:56:49 -0800

    Add size.py.

---
+++ size.py
@@ -0,0 +1,17 @@
+import sys
+
+from voussoirkit import bytestring
+from voussoirkit import spinal
+from voussoirkit import pathclass
+
+paths = sys.argv[1:]
+paths = [pathclass.Path(p) for p in paths]
+
+total = 0
+for path in paths:
+    if path.is_file:
+        total += path.size
+    elif path.is_dir:
+        total += spinal.get_dir_size(path)
+
+print(total)
