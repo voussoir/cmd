d00d9369bbd64ea39444d92ff248717793e7a4ff
Ethan Dalool <git@voussoir.net>
2020-08-20 10:58:09 -0700

    Add directory_discrepancy.py.

---
+++ directory_discrepancy.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+dir1 = sys.argv[1]
+dir2 = sys.argv[2]
+
+files1 = set(os.listdir(dir1))
+files2 = set(os.listdir(dir2))
+
+print(f'In "{dir1}" but not in "{dir2}":')
+print('=============================')
+for discrepancy in files1.difference(files2):
+    print(discrepancy)
+
+print()
+
+print(f'In "{dir2}" but not in "{dir1}":')
+print('=============================')
+for discrepancy in files2.difference(files1):
+    print(discrepancy)
