6f53cb6d8a51cb1d03ff1d7dfccbeab82b9582e3
Ethan Dalool <git@voussoir.net>
2020-09-11 17:03:31 -0700

    Fix move_all.

--- move_all.py
+++ move_all.py
@@ -20,8 +20,9 @@ destination = pathclass.Path(sys.argv[-1])
 if not destination.is_dir:
     raise TypeError(destination)
 
-if any(destination.with_child(file.basename).exists for file in files):
-    raise Exception(file.basename)
+for file in files:
+    if destination.with_child(file.basename).exists:
+        raise Exception(file.basename)
 
 for file in files:
     new_path = destination.with_child(file.basename)
