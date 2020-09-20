58e50976398bc5a08e900ecc845c02f1a3bcb3d7
Ethan Dalool <git@voussoir.net>
2020-09-20 13:18:30 -0700

    Use argparse for grayscale.py.

--- grayscale.py
+++ grayscale.py
@@ -1,3 +1,4 @@
+import argparse
 import PIL.Image
 import sys
 
@@ -5,17 +6,37 @@ from voussoirkit import pathclass
 from voussoirkit import pipeable
 from voussoirkit import winglob
 
+def grayscale(filename, *, inplace=False):
+    filename = pathclass.Path(filename)
+
+    basename = filename.replace_extension('').basename
+    if basename.endswith('_gray'):
+        return
+
+    if inplace:
+        new_filename = filename
+    else:
+        basename += '_gray'
+        new_filename = filename.parent.with_child(basename).add_extension(filename.extension)
+
+    image = PIL.Image.open(filename.absolute_path).convert('LA')
+    print(f'{new_filename.relative_path}')
+    image.save(new_filename.absolute_path)
+
+def grayscale_argparse(args):
+    filenames = winglob.glob(args.pattern)
+    for filename in filenames:
+        return grayscale(filename, inplace=args.inplace)
+
 def main(argv):
-    for pattern in pipeable.go(argv, strip=True, skip_blank=True):
-        filenames = winglob.glob(pattern)
-        for filename in filenames:
-            filename = pathclass.Path(filename)
-            if filename.replace_extension('').basename.endswith('_gray'):
-                continue
-            new_filename = filename.replace_extension('').absolute_path + '_gray' + filename.dot_extension
-            print(f'{filename.basename} -> {new_filename.basename}')
-            image = PIL.Image.open(filename.absolute_path).convert('LA')
-            image.save(new_filename)
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('pattern')
+    parser.add_argument('--inplace', dest='inplace', action='store_true')
+    parser.set_defaults(func=grayscale_argparse)
+
+    args = parser.parse_args(argv)
+    return args.func(args)
 
 if __name__ == '__main__':
     raise SystemExit(main(sys.argv[1:]))
