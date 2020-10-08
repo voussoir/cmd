c4670157c5bb5030b00de39c39936f1b18396d42
Ethan Dalool <git@voussoir.net>
2020-10-08 10:03:08 -0700

    Remove .exe from psexec in case it is an lnk or something.

--- sudo_task_scheduler.bat
+++ sudo_task_scheduler.bat
@@ -1,3 +1,3 @@
 rem Run as administrator!
 rem https://docs.microsoft.com/en-us/sysinternals/downloads/psexec
-PsExec.exe -i -s %windir%\system32\mmc.exe /s taskschd.msc
+PsExec -i -s %windir%\system32\mmc.exe /s taskschd.msc
