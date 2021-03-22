87376dcd4c1f6ceea15493988aedcd4ae81c0f4b
Ethan Dalool <git@voussoir.net>
2021-03-21 17:28:30 -0700

    Add stderr.py, stdout.py.

---
+++ stdout.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+
+from voussoirkit import pipeable
+from voussoirkit import vlogging
+
+log = vlogging.getLogger(__name__, 'printstdout')
+
+def printstdout_argparse(args):
+    for text in args.texts:
+        text = pipeable.input(text)
+        for line in text:
+            pipeable.stdout(line)
+
+def main(argv):
+    argv = vlogging.set_level_by_argv(log, argv)
+
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('texts', nargs='+')
+    parser.set_defaults(func=printstdout_argparse)
+
+    args = parser.parse_args(argv)
+    return args.func(args)
+
+if __name__ == '__main__':
+    raise SystemExit(main(sys.argv[1:]))
