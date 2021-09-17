9c297315fe07394a6c7c65c60f3ee441c9855861
Ethan Dalool <git@voussoir.net>
2021-09-17 14:59:36 -0700

    Show enabled and disabled packages separately.

--- adb_ls.bat
+++ adb_ls.bat
@@ -1,1 +1,11 @@
-adb shell pm list packages -f
+@echo off
+
+echo ENABLED
+echo =======
+adb shell pm list packages -e | sorted !i
+
+echo.
+
+echo DISABLED
+echo ========
+adb shell pm list packages -d | sorted !i
