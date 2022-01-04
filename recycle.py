043505f80aa25cd167816e5715db1356f1efea94
Ethan Dalool <git@voussoir.net>
2022-01-04 12:43:37 -0800

    Print the file which raised an exception.

--- recycle.py
+++ recycle.py
@@ -7,7 +7,12 @@ from voussoirkit import pipeable
 def main(argv):
     for path in pathclass.glob_many(pipeable.go(argv, skip_blank=True)):
         pipeable.stdout(path.absolute_path)
-        send2trash.send2trash(path)
+        try:
+            send2trash.send2trash(path)
+        except Exception as exc:
+            pipeable.stderr(f'Recycling {path.absolute_path} caused an exception:')
+            pipeable.stderr(str(exc))
+            return 1
     return 0
 
 if __name__ == '__main__':
