d97b32f250d39a6b4d11b0d52b7f0bb21dfef614
Ethan Dalool <git@voussoir.net>
2025-11-23 14:46:52 -0800

    Use encode with replace to tolerate some breakage.

--- cup.py
+++ cup.py
@@ -1,6 +1,5 @@
 import pyperclip
 
 from voussoirkit import pipeable
-
 text = pipeable.input('!i', split_lines=False)
-pyperclip.copy(text)
+pyperclip.copy(text.encode('utf-8', 'replace').decode())
